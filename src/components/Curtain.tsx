a3fb01e77f863a4ac8d60f24957853a38f43721f
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-04-27 23:34:51 +1000

    Add navbar

---
+++ src/components/Curtain.tsx
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion'
+
+type CurtainProps = {
+  isOpen: boolean
+}
+
+const variants = {
+  open: {
+    height: '100vh',
+    opacity: 0.9,
+    backgroundColor: '#00020d',
+  },
+  closed: {
+    height: 0,
+  },
+}
+
+export default function Curtain({ isOpen }: CurtainProps) {
+  return (
+    <motion.section
+      className="fixed top-0 right-0 bottom-auto left-0"
+      initial="closed"
+      animate={isOpen ? 'open' : 'closed'}
+      transition={{ duration: 0.5 }}
+      variants={variants}
+    />
+  )
+}
---
+++ src/components/Navigations.tsx
@@ -0,0 +1,26 @@
+export const navigations = [
+  {
+    id: 0,
+    title: 'Home',
+    path: '/',
+    delay: 1.5,
+  },
+  {
+    id: 1,
+    title: 'Blog',
+    path: '/blog',
+    delay: 2,
+  },
+  {
+    id: 2,
+    title: 'Tools',
+    path: '/tools',
+    delay: 2.5,
+  },
+  {
+    id: 3,
+    title: 'Projects',
+    path: '/project',
+    delay: 3,
+  },
+]
