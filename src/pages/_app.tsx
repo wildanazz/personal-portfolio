f25b981cf02ee2dd45641c66f93d9123126853cf
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-04-28 23:39:21 +1000

    Add link for social media

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/Navbar'
+import Social from '@/components/Social'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <Social />
       <Component {...pageProps} />
     </>
   )
