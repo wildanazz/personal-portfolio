171d199e5513b238f649804aec89f2f1255bd583
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-05-14 05:52:18 +1000

    Change to default font style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,20 @@
 import '@/styles/globals.css'
-import localFont from 'next/font/local'
+// import localFont from 'next/font/local'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from 'framer-motion'
 import Navbar from '@/components/Navbar'
 import Social from '@/components/Social'
 
-const myFont = localFont({
-  src: [{ path: './font/iosevka-term-light.woff2', weight: '300' }],
-  adjustFontFallback: false,
-  fallback: ['monospace'],
-})
+// const myFont = localFont({
+//   src: [{ path: './font/iosevka-term-light.woff2', weight: '300' }],
+//   adjustFontFallback: false,
+//   fallback: ['monospace'],
+// })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={myFont.className}>
+    // <main className={myFont.className}>
+    <main>
       <AnimatePresence>
         <Component {...pageProps} />
       </AnimatePresence>
