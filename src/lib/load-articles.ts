d3b983a2c58585e023218e3a1bff4dfb06f4a598
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-05-14 03:12:12 +1000

    Remove revalidate and add cache

--- src/lib/load-articles.ts
+++ src/lib/load-articles.ts
@@ -4,7 +4,6 @@ export async function getArticlesFromAPI(): Promise<IArticle[]> {
   const response = await fetch('https://dev.to/api/articles/me/published', {
     method: 'GET',
     headers: { 'api-key': `${process.env.DEV_API_KEY}` },
-    next: { revalidate: 300 },
   })
   const articles = await response.json()
 
