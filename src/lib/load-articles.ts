3e80e82c0e335db63fb4ff8f92c4d9fb4963711e
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-05-14 04:35:59 +1000

    Add error handling

--- src/lib/load-articles.ts
+++ src/lib/load-articles.ts
@@ -4,8 +4,15 @@ export async function getArticlesFromAPI(): Promise<IArticle[]> {
   const response = await fetch('https://dev.to/api/articles/me/published', {
     method: 'GET',
     headers: { 'api-key': `${process.env.DEV_API_KEY}` },
-    next: { revalidate: 10 },
   })
+
+  if (!response.ok) {
+    // If there is a server error, you might want to
+    // throw an error instead of returning so that the cache is not updated
+    // until the next successful request.
+    throw new Error(`Failed to fetch posts, received status ${response.status}`)
+  }
+
   const articles = await response.json()
 
   return articles
