90e164191740a7060b4f29650969fda17f08d932
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-05-14 02:38:13 +1000

    Fix endpoint

--- src/lib/load-articles.ts
+++ src/lib/load-articles.ts
@@ -1,7 +1,7 @@
 import IArticle from '@/interfaces/IArticle'
 
 export async function getArticlesFromAPI(): Promise<IArticle[]> {
-  const response = await fetch('https://dev.to/api/articles/me', {
+  const response = await fetch('https://dev.to/api/articles/me/published', {
     method: 'GET',
     headers: { 'api-key': `${process.env.DEV_API_KEY}` },
     next: { revalidate: 300 },
