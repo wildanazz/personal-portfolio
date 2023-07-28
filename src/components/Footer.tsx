3213828a7231733c77a6054de59b90645c62f29c
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-07-28 20:15:56 +0700

    Add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="w-full flex flex-col items-center h-[90px] mt-12 " />
+    <footer className="w-full flex flex-row items-center justify-center h-[90px] mt-12 text-black dark:text-white gap-2.5">
+      © 2023 M Wildan Aziz. |
+      <a
+        className="text-md text-black dark:text-white"
+        href="mailto:wildanazzwa@gmail.com?Subject=Hello"
+      >
+        wildanazzwa@gmail.com
+      </a>
+    </footer>
   )
 }
