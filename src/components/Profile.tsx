4419dc403ee7e50d9b6f913d11ae44afa8054e27
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-08-11 00:08:47 +0700

    Remove the title name

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 
 export default function Profile() {
   return (
-    <Image
-      alt="me"
-      src={'https://wildanazz.blob.core.windows.net/profile/Me.JPG'}
-      width={48}
-      height={48}
-      className="rounded-full"
-    />
+    <Link href="/">
+      <Image
+        alt="me"
+        src={'https://wildanazz.blob.core.windows.net/profile/Me.JPG'}
+        width={48}
+        height={48}
+        className="rounded-full"
+      />
+    </Link>
   )
 }
