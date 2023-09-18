ba8a94d1202be545cf9e3d267c599608a4008a3a
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2023-09-18 17:26:01 +0700

    Add cover image

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   images: {
-    domains: ['wildanazz.azureedge.net', 'mosaic.scdn.co', 'i.scdn.co'],
+    domains: [
+      'wildanazz.azureedge.net',
+      'mosaic.scdn.co',
+      'i.scdn.co',
+      'res.cloudinary.com',
+    ],
   },
 }
 
