286a27d79658eca465454b59dafb3ab287ddea8f
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2025-01-16 13:47:09 +0700

    Fix layout animation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,9 +13,12 @@ export default function Layout({ children }: Props) {
         <Background />
       </div>
       <motion.div
-        initial={{ x: -200, y: 0, opacity: 0 }}
-        animate={{ x: 0, y: 0, opacity: 1 }}
-        exit={{ x: 0, y: -100, opacity: 0 }}
+        // initial={{ x: -200, y: 0, opacity: 0 }}
+        // animate={{ x: 0, y: 0, opacity: 1 }}
+        // exit={{ x: 0, y: -100, opacity: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
         transition={{ type: 'linear' }}
       >
         <main className="flex flex-col items-start pt-24 px-8 sm:px-16 md:px-36 lg:px-52 xl:px-80 2xl:px-96 overflow-hidden">
